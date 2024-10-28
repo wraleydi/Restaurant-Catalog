fd6f068aada8721350ffc065f1577699fccd9579
wraleydi <raleydi06@gmail.com>
2024-10-28 12:56:34 +0700

    membuat daftar restoran dan footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import './view/restaurant.js'
 
 console.log('Hello Coders! :)');
---
+++ src/scripts/view/restaurant.js
@@ -0,0 +1,32 @@
+import {restaurantApi} from '../remote/api-data.js'
+
+function displayMenu(restaurant) {
+    const itemMenu = document.createElement('div')
+    itemMenu.classList.add('card')
+
+    itemMenu.innerHTML = `
+    <h2>${restaurant.name}</h2>
+    <img src="${restaurant.pictureId}" alt="${restaurant.name}">
+        <p><strong>City:</strong> ${restaurant.city}</p>
+        <p><strong>⭐Rating:</strong> ${restaurant.rating}</p>
+    `
+    return itemMenu
+}
+
+function renderMenu(restaurants) {
+    const container = document.getElementById('container-item')
+    container.innerHTML = "";
+
+    restaurants.forEach(restaurant => {
+        const itemMenu = displayMenu(restaurant)
+        container.appendChild(itemMenu)
+    });
+}
+
+async function loadRestaurants() {
+    const restaurants = await restaurantApi()
+    console.log(restaurants);
+    renderMenu(restaurants)
+}
+
+loadRestaurants()
