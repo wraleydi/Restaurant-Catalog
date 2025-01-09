27e13cb9901c3e5592b384e8df81505733f01faa
wraleydi <raleydi06@gmail.com>
2025-01-10 05:43:57 +0700

    end test

--- e2e/Liking_Restaurants.test.js
+++ e2e/Liking_Restaurants.test.js
@@ -2,40 +2,37 @@ const assert = require('assert');
 
 Feature('Liking and Unliking Restaurants');
 
-Scenario('liking one restaurant from catalog', async ({ I }) => {
+Scenario('Liking and Unliking restaurant from favorite', async ({ I }) => {
     I.amOnPage('/');
     I.see('Catalog', '.title-catalog');
     I.seeElement('.card');
-  
     I.seeElement('.restaurant_detail a');
+
     const firstRestaurant = locate('.restaurant_detail a').first();
     const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+
     I.click(firstRestaurant);
-  
+
     I.seeElement('#likeButton');
     I.click('#likeButton');
-  
-    I.wait(2);
+
     I.amOnPage('/#/favorite');
-    I.seeElement('.card');
-    const likedRestaurantTitle = await I.grabTextFrom('.restaurant_detail a');
-  
-    assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+
+    const visibleRestaurants = await I.grabNumberOfVisibleElements('.restaurant_detail a');
+    if (visibleRestaurants > 0) {
+        const likedRestaurantTitle = await I.grabTextFrom('.restaurant_detail a');
+        assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+
+        I.click('.restaurant_detail a');
+        I.seeElement('#likeButton');
+        I.click('#likeButton');
+
+        I.amOnPage('/#/favorite');
+        const updatedRestaurants = await I.grabNumberOfVisibleElements('.restaurant_detail a');
+        assert.strictEqual(updatedRestaurants, 0, 'No restaurants should remain in favorites');
+    } else {
+        I.see('No Favorite Restaurant', '.message_text');
+    }
 });
 
-Scenario('Unliking restaurant from favorite', async ({ I }) => {
-    I.amOnPage('/#/favorite');
-    I.see('Your Favorite Restaurant', '.title-catalog');
-    
-    I.seeElement('.restaurant_detail a');
-    const firstRestaurant = locate('.restaurant_detail a').first();
-    const titleRestaurant = await I.grabTextFrom(firstRestaurant);
-    I.click(firstRestaurant);
 
-    I.seeElement('#likeButton .fa.fa-heart');
-    I.click('#likeButton .fa.fa-heart');
-  
-    I.amOnPage('/#/favorite');
-    const isRestaurantPresent = await I.grabTextFromAll('.card .title_restaurant');
-    I.assertNotInclude(isRestaurantPresent, titleRestaurant, 'Restaurant should be removed from favorites');
-});
